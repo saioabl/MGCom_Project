12bd9a3a476d0f279c451424b3ebfc91376683cb
NET\saioa <saioa.buruchaga@chalmers.se>
2018-01-29 17:09:25 +0100

    Added parameters for Current Controller and PLL according to the calculations in the book of Lennart Harnefors

--- VSC_Grid_MyModel/VSC_Grid_Data_MyModel.m
+++ VSC_Grid_MyModel/VSC_Grid_Data_MyModel.m
@@ -11,7 +11,7 @@ close all
 % *************************************************************************
 Ts = 2e-4; % s, sampling time
 fs = 1/Ts; % Hz, sampling frequency
-fsw = fs; % Hz, switching frequency
+fsw = fs;  % Hz, switching frequency
 
 
 % *************************************************************************
@@ -20,17 +20,17 @@ fsw = fs; % Hz, switching frequency
 
 % DC-link
 C_dc = 5000e-06; % F, dc-link capacitor
-V_dc = 680; % V, dc voltage source
-R_dc = 1e-5; % ohms, dc-link resitance
+V_dc = 680;      % V, dc voltage source
+R_dc = 1e-5;     % ohms, dc-link resitance
 
 % IGBT bridge
-Rs = 5000; % ohms, snubber resitance IGBT
-Cs = inf; % F; snubber capacitance IGBT
-Ron = 1e-3; % ohms, conducting resistance IGBT
+Rs = 5000;       % ohms, snubber resitance IGBT
+Cs = inf;        % F; snubber capacitance IGBT
+Ron = 1e-3;      % ohms, conducting resistance IGBT
 
 % Filter
-Rf=6e-3; % ohm, filter resistance
-Lf=2e-3; % H, filter inductance
+Rf=6e-3;         % ohm, filter resistance
+Lf=2e-3;         % H, filter inductance
 
 
 % *************************************************************************
@@ -38,6 +38,25 @@ Lf=2e-3; % H, filter inductance
 % *************************************************************************
 
 %  Voltage source
-fn = 50; % Hz, rated frequency
-wn = 2*pi*fn; % rad/s, rated angular frequency
+fn = 50;               % Hz, rated frequency
+wn = 2*pi*fn;          % rad/s, rated angular frequency
 VLL_rms = 230*sqrt(3); % V
+
+
+% *************************************************************************
+% Current Controller (cc)-- L.H book
+% *************************************************************************
+
+alpha_cc = 2*pi*fs/10; % rad/s, bandwidth (2*pi*500)
+Ki_cc = alpha_cc*Rf;   % Integral gain
+Kp_cc = alpha_cc*Lf;   % Proportional gain 
+
+
+% *************************************************************************
+% PLL -- L.H book
+% *************************************************************************
+
+alpha_PLL = 2*pi*fs/1000;  % rad/s, bandwidth (alpha_cc/100)
+Ki_PLL = alpha_PLL^2;      % Integral gain
+Kp_PLL = alpha_PLL*2;      % Proportional gain
+samp_delay = 1.5*Ts;       % phase delay b.of discretization 1*Ts + 0.5 Ts
